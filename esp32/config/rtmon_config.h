7c1d131195fa37fdd770b12ab7be185bc8a40227
Roman Egoshin <egoshin.roman@gmail.com>
2025-11-04 23:31:01 +0100

    Fix esp32/esp8266 examples. Update <rt-monitor> submodule

--- esp32/config/rtmon_config.h
+++ esp32/config/rtmon_config.h
@@ -2,24 +2,37 @@
 #ifndef RTMON_CONFIG_H_
 #define RTMON_CONFIG_H_
 
-//#include "sdkconfig.h"
+#include "sdkconfig.h"
 
 #define RTMON_CFG_USE_STATIC_ALOCATION      ( 0 )
 
-#define RTMON_CFG_TASK_STACK_DEPTH          ( 256 )
+#define RTMON_CFG_TASK_STACK_DEPTH          ( 2 * 1024 )
 
-#define RTMON_CFG_TASKS_MAX_COUNT           ( 8 )
+#define RTMON_CFG_TASKS_MAX_COUNT           ( 16 )
 
 #define RTMON_CFG_UPDATE_PERIOD_MS          ( 1000 )
 
 #define RTMON_CFG_LINE_BUFF_SIZE            ( 64 )
 
-#define RTMON_CFG_PERCENT_SCALE             ( 100 )
+#define RTMON_CFG_PERCENT_SCALE             ( 10 )
 
-#define RTMON_CFG_VIEW_DEBUG_INFO           ( 0 )
+#define RTMON_CFG_VIEW_DEBUG_INFO           ( 1 )
 
 #define RTMON_CFG_UART_NUM                  ( UART_NUM_2 )
 #define RTMON_CFG_TX_PIN                    ( GPIO_NUM_17 )
-#define RTMON_CFG_RX_PIN                    ( GPIO_NUM_16  )
+
+// Compatible defines:
+#if !CONFIG_FREERTOS_SMP
+#define TICK_TYPE_WIDTH_16_BITS     0
+#define TICK_TYPE_WIDTH_32_BITS     1
+#define TICK_TYPE_WIDTH_64_BITS     2
+#if CONFIG_FREERTOS_RUN_TIME_COUNTER_TYPE_U32
+  #define configRUN_TIME_COUNTER_TYPE    uint32_t
+  #define configRUN_TIME_TYPE_WIDTH      TICK_TYPE_WIDTH_32_BITS
+#elif CONFIG_FREERTOS_RUN_TIME_COUNTER_TYPE_U64
+  #define configRUN_TIME_COUNTER_TYPE    uint64_t
+  #define configRUN_TIME_TYPE_WIDTH      TICK_TYPE_WIDTH_64_BITS
+  #endif /* CONFIG_FREERTOS_RUN_TIME_COUNTER_TYPE_U64 */
+#endif /* !CONFIG_FREERTOS_SMP */
 
 #endif /* RTMON_CONFIG_H_ */
--- esp8266/config/rtmon_config.h
+++ esp8266/config/rtmon_config.h
@@ -6,17 +6,17 @@
 
 #define RTMON_CFG_USE_STATIC_ALOCATION      ( 0 )
 
-#define RTMON_CFG_TASK_STACK_DEPTH          ( 256 )
+#define RTMON_CFG_TASK_STACK_DEPTH          ( 2048 )
 
-#define RTMON_CFG_TASKS_MAX_COUNT           ( 8 )
+#define RTMON_CFG_TASKS_MAX_COUNT           ( 16 )
 
 #define RTMON_CFG_UPDATE_PERIOD_MS          ( 1000 )
 
 #define RTMON_CFG_LINE_BUFF_SIZE            ( 64 )
 
-#define RTMON_CFG_PERCENT_SCALE             ( 100 )
+#define RTMON_CFG_PERCENT_SCALE             ( 10 )
 
-#define RTMON_CFG_VIEW_DEBUG_INFO           ( 0 )
+#define RTMON_CFG_VIEW_DEBUG_INFO           ( 1 )
 
 #define RTMON_CFG_UART_NUM                  ( UART_NUM_1 )
 
@@ -24,10 +24,20 @@
 #define TICK_TYPE_WIDTH_16_BITS     0
 #define TICK_TYPE_WIDTH_32_BITS     1
 #define TICK_TYPE_WIDTH_64_BITS     2
+
+#ifdef CONFIG_FREERTOS_RUN_TIME_STATS_USING_CPU_CLK
+#define configRUN_TIME_TYPE_WIDTH           TICK_TYPE_WIDTH_64_BITS
+#define configRUN_TIME_COUNTER_TYPE         uint64_t
+#else
 #define configRUN_TIME_TYPE_WIDTH           TICK_TYPE_WIDTH_32_BITS
 #define configRUN_TIME_COUNTER_TYPE         uint32_t
+#endif
 
 #define xPortGetFreeHeapSize()              esp_get_free_heap_size()
 #define xPortGetMinimumEverFreeHeapSize()   esp_get_minimum_free_heap_size()
+
+#if defined(CONFIG_FREERTOS_RUN_TIME_STATS_USING_ESP_TIMER)
+#define portGET_RUN_TIME_COUNTER_VALUE()    esp_get_time()
+#endif
 
 #endif /* RTMON_CONFIG_H_ */
